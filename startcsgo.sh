f02dea98cafa6ed57ad18b83c7c160300738a814
joaaoleite <joaogsleite@gmail.com>
2017-01-06 18:07:59 +0000

    start cs

---
+++ startcsgo.sh
@@ -0,0 +1,6 @@
+
+# for testing
+cat
+
+# real world
+# ./srcds_run -game csgo -usercon +game_type 0 +game_mode 1 +mapgroup mg_bomb +map de_dust2 +sv_setsteamaccount DD5927852743618A077A5E337D7387C1 -net_port_try 1
