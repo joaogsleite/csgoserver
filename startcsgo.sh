38a24e9c8c13c542cb70b14f8eb1cd8a87bed78e
joaaoleite <joaogsleite@gmail.com>
2017-01-07 01:06:39 +0000

    ip

--- startcsgo.sh
+++ startcsgo.sh
@@ -3,4 +3,4 @@
 #cat
 
 # real world
-/home/steam/Steam/csgo-ds/srcds_run -game csgo -usercon +game_type 0 +game_mode 1 +mapgroup mg_bomb +map de_dust2 +sv_setsteamaccount DD5927852743618A077A5E337D7387C1 -authkey F38BF2FDE5230266744967D0585BD66D -net_port_try 1
+/home/steam/Steam/csgo-ds/srcds_run -game csgo -usercon +game_type 0 +game_mode 1 +mapgroup mg_bomb +map de_dust2 +sv_setsteamaccount DD5927852743618A077A5E337D7387C1 +ip 00.00.00.00 -authkey F38BF2FDE5230266744967D0585BD66D -net_port_try 1
